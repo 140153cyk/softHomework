ebccae8c196ea63733ef7ee280e52f3a5992b8b7
140153cyk <99869808+140153cyk@users.noreply.github.com>
2022-02-19 13:02:33 +0800

    Add files via upload

---
+++ 第一次作业/work1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace work1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter an operand");
+            int x=Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter another operand");
+            int y = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("please choose an operator");
+            String o = Console.ReadLine();
+            int result;
+            switch (o)
+            {
+                case "+":result = x + y;break;
+                case "-":result = x - y;break;
+                case "*":result = x * y;break;
+                case "/":result = x / y;break;
+                default:result = 0;break;
+            }
+
+            Console.WriteLine("result is {0}", result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 第一次作业/work2/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace work2
+{
+    public partial class Form1 : Form
+    {
+        int x = 0, y = 0,result=0;
+        int o=0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+             x = Int32.Parse(tb.Text);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+             y = Int32.Parse(tb.Text);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListBox lb = (ListBox)sender;
+            o = lb.SelectedIndex;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch (o)
+            {
+                case 0:result = x + y;break;
+                case 1:result = x - y;break;
+                case 2:result = x * y;break;
+                case 3:result = x / y;break;
+            }
+            label2.Text = "" + result;
+        }
+    }
+}
